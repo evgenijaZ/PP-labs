b1d94636f4387a18d1659a67b5712ac9d36a9c6e
evgenijaZ <evg.zubrich@gmail.com>
2017-09-09 23:17:03 +0300

    created template of project

--- lab1/src/lab1.adb
+++ lab1/src/lab1.adb
@@ -1,20 +1,25 @@
-with Ada.Text_IO;
-use Ada.Text_IO;
-procedure Lab1 is
-
+with Ada.Text_IO; use Ada.Text_IO;
+with Data; use Data;
+procedure lab1 is
+ 
    task T1;
    task body T1 is
    begin
-      Put("T1");
+      Put_Line("Task 1:");
    end T1;
-
-    task T2;
+   
+   task T2;
    task body T2 is
    begin
-      Put("T2");
+      Put_Line("Task 2:");
    end T2;
-
+   
+   task T3;
+   task body T3 is
+   begin
+      Put_Line("Task 3:");
+   end T3;
+   
 begin
-   Put_Line("Lab1");
-   null;
-end Lab1;
+   Put_Line("lab1");
+end lab1;
