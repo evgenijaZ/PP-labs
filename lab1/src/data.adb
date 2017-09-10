9b4c5531540308b67bc2b2f4d775afacb4160f60
evgenijaZ <evg.zubrich@gmail.com>
2017-09-10 22:28:16 +0300

    added main functions for working with matrix and vector

--- lab1/src/data.adb
+++ lab1/src/data.adb
@@ -1,3 +1,7 @@
+with Ada.Integer_Text_IO;
+with Ada.Integer_Text_IO;
+with Ada.Text_IO; use Ada.Text_IO;
+
 package body Data is
 
    function F1(A : in Matrix; D : in Matrix; B : in Vector) return Matrix is
@@ -17,5 +21,118 @@ package body Data is
    begin
       return O;
    end F3;
+
+   procedure Matrix_Input (A : out Matrix) is
+      item : Integer;
+   begin
+      Put_Line ("Enter matrix values:");
+      for i in 1..N loop
+         for j in 1..N loop
+            Ada.Integer_Text_IO.Get (item);
+            A(i,j) := item;
+         end loop;
+      end loop;
+   end Matrix_Input;
+
+   procedure Matrix_Output(A : in Matrix) is
+   begin
+      Put_Line ("Matrix:");
+      for i in 1..N loop
+         for j in 1..N loop
+            Ada.Integer_Text_IO.Put(A(i,j));
+         end loop;
+         New_Line;
+      end loop;
+   end Matrix_Output;
+
+
+   procedure Vector_Input (A : out Vector) is
+      item : Integer;
+   begin
+      Put_Line ("Enter vector values:");
+      for i in 1..N loop
+         Ada.Integer_Text_IO.Get(item);
+         A(i) := item;
+      end loop;
+   end Vector_Input;
+
+   procedure Vector_Output(A : in Vector) is
+   begin
+      Put_Line ("Vector:");
+      for i in 1..N loop
+         Ada.Integer_Text_IO.Put(A(i));
+      end loop;
+      New_Line;
+   end Vector_Output;
+
+   function Max (A: Vector) return Integer is
+      x : Integer;
+   begin
+      x := A(1);
+      for i in 1..N loop
+         if A(i)>x then
+            x := A(i);
+         end if;
+      end loop;
+      return x;
+   end Max;
+
+   function Amount ( A: in Vector; B: in Vector) return Vector is
+      C : Vector;
+   begin
+      for i in 1..N loop
+         C(i):=A(i)+B(i);
+      end loop;
+      return C;
+   end Amount;
+
+   function Multiple ( A: in Integer; B: in out Matrix) return Matrix is
+      C:Matrix;
+   begin
+      for i in 1..N loop
+         for j in 1..N loop
+            C(i,j):=A*B(i,j);
+         end loop;
+      end loop;
+      return C;
+   end Multiple;
+
+   function Trans (A: in Matrix) return Matrix is
+      B:Matrix;
+   begin
+      for i in 1..N loop
+         for j in 1..N loop
+            B(i,j):=A(j,i);
+         end loop;
+      end loop;
+      return B;
+   end Trans;
+
+   function Amount ( A: in Matrix; B: in Matrix) return Matrix is
+      C:Matrix;
+   begin
+      for i in 1..N loop
+         for j in 1..N loop
+           C(i,j):=A(i,j)+B(i,j);
+         end loop;
+      end loop;
+      return C;
+   end Amount;
+
+
+   function Multiple ( A: in Matrix; B: in Matrix) return Matrix is
+      C:Matrix;
+   begin
+      for row in 1..N loop
+         for col in 1..N loop
+            C(row,col):=0;
+          for inner in 1..N loop
+            C(row,col) := C(row,col) + A(row,inner) * B(inner,col);
+          end loop;
+         end loop;
+      end loop;
+      return C;
+   end Multiple;
+
 
 end Data;
--- lab1/src/data.ads
+++ lab1/src/data.ads
@@ -1,14 +1,23 @@
 package Data is
-   N : Integer;
-
-   type Vector is array (1..N) of Integer;
+   N : constant := 3;
+   
+   type Vector is array(1..N) of Integer;
    type Matrix is array (1..N,1..N) of Integer;
    
-   --procedure Matrix_Input();
-   --procedure Matrix_Output();
+   procedure Vector_Input(A : out Vector);
+   procedure Vector_Output(A : in Vector);
+   procedure Matrix_Input(A : out Matrix);
+   procedure Matrix_Output(A : in Matrix);
    
    function F1(A : in Matrix; D : in Matrix; B : in Vector) return Matrix;
    function F2(a : in Integer; G : in Matrix; K : in Matrix; L : in Matrix) return Matrix;
    function F3(P : in Matrix; R : in Matrix; S : in Vector; T : in Vector) return Vector;
    
+   function Amount ( A: in Vector; B: in Vector) return Vector;
+   function Amount ( A: in Matrix; B: in Matrix) return Matrix;
+   function Multiple ( A: in Integer; B: in out Matrix) return Matrix;
+   function Multiple ( A: in Matrix; B: in Matrix) return Matrix;
+   function Max (A: Vector) return Integer;
+   function Trans (A: in Matrix) return Matrix;
+      
 end Data;
