309ddda0d5d3b042301d47657d1b594d8d9ef42c
evgenijaZ <evg.zubrich@gmail.com>
2017-09-10 22:29:13 +0300

    some specifies

--- lab1/src/lab1.adb
+++ lab1/src/lab1.adb
@@ -1,25 +1,30 @@
-with Ada.Text_IO; use Ada.Text_IO;
-with Data; use Data;
+with Ada.Text_IO;
+use Ada.Text_IO;
+with Data;
+use Data;
+with Ada.Integer_Text_IO;
+with Ada.Integer_Text_IO;
 procedure lab1 is
- 
+
    task T1;
    task body T1 is
    begin
-      Put_Line("Task 1:");
+      Put_Line ("Task 1:");
    end T1;
-   
+
    task T2;
    task body T2 is
    begin
-      Put_Line("Task 2:");
+      Put_Line ("Task 2:");
    end T2;
-   
+
    task T3;
    task body T3 is
    begin
-      Put_Line("Task 3:");
+      Put_Line ("Task 3:");
    end T3;
-   
+
+
 begin
-   Put_Line("lab1");
+
 end lab1;
