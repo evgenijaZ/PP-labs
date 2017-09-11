fd3221609a1ee0f0b1db05cf1923c90aa80fff5f
evgenijaZ <evg.zubrich@gmail.com>
2017-09-11 17:15:42 +0300

    function for generating value

--- lab1/src/data.ads
+++ lab1/src/data.ads
@@ -3,15 +3,14 @@ package Data is
    NMax : constant := 100;
    type Vector is array(1..NMax) of Integer;
    type Matrix is array (1..NMax,1..NMax) of Integer;
-   
-
-   
+      
    procedure Vector_Input(A : out Vector);
    procedure Vector_Output(A : in Vector);
    procedure Vector_Generate (A : out Vector);
    procedure Matrix_Input(A : out Matrix);
    procedure Matrix_Output(A : in Matrix);
    procedure Matrix_Generate (A : out Matrix);
+   procedure Value_Generate (A : out Integer);
    
    function F1(A : in Matrix; D : in Matrix; B : in Vector) return Matrix;
    function F2(a : in Integer; G : in Matrix; K : in Matrix; L : in Matrix) return Matrix;
@@ -20,10 +19,9 @@ package Data is
    function Amount ( A: in Vector; B: in Vector) return Vector;
    function Amount ( A: in Matrix; B: in Matrix) return Matrix;
    function Multiple ( A: in Integer; B: in out Matrix) return Matrix;
-   function Multiple ( A: in Matrix; B: in Matrix) return Matrix;
+   function Multiple ( A: in Matrix; B: in Matrix) return Matrix;  
+   function Multiple ( A: in Matrix; B: in Vector) return Vector;
    function Max (A: Vector) return Integer;
    function Trans (A: in Matrix) return Matrix;
-   
-
-   
+      
 end Data;
