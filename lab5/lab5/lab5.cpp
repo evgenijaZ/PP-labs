463445074c783704375245af6d927ba6ed88872f
evgenijaZ <evg.zubrich@gmail.com>
2017-11-03 12:13:31 +0200

    added critical sections

--- lab5/lab5/lab5.cpp
+++ lab5/lab5/lab5.cpp
@@ -21,50 +21,69 @@ int main()
 		{
 #pragma omp section
 			{
-				cout << "Thread #1 started" << endl;
+#pragma omp critical
+				{
+					cout << "Thread #1 started" << endl;
+				}
 				DataClass Manager(N);
 				Matrix A(N);
 				Matrix D(N);
 				Vector B(N);
 				Matrix E(N);
-				cout << endl << "Matrix E (thread #1):" << endl;
 				E = Manager.F1(A, D, B);
-				E.Output();
-				cout << endl << "Thread #1 finished" << endl;
+#pragma omp critical
+				{
+					cout << endl << "Matrix E (thread #1):" << endl;
+					E.Output();
+					cout << endl << "Thread #1 finished" << endl;
+				}
 
 			}
 #pragma omp section
 			{
-				cout << endl << "Thread #2 started" << endl;
+#pragma omp critical
+				{
+					cout << "Thread #2 started" << endl;
+				}
 				DataClass Manager(N);
 				Matrix G(N);
 				Matrix K(N);
 				Matrix L(N);
-				cout << endl << "Matrix F (thread #2):" << endl;
 				Matrix F = Manager.F2(5, G, K, L);
-				F.Output();
-				cout << endl << "Thread #2 finished" << endl;
+#pragma omp critical
+				{
+					cout << endl << "Matrix F (thread #2):" << endl;
+					F.Output();
+					cout << endl << "Thread #2 finished" << endl;
+				}
 
 			}
 #pragma omp section
 			{
-				cout << endl << "Thread #3 started" << endl;
+#pragma omp critical
+				{
+					cout << "Thread #3 started" << endl;
+				}
 				DataClass Manager(N);
 				Matrix P(N);
 				Matrix R(N);
 				Vector S(N);
 				Vector T(N);
-				cout << endl << "Vector O (thread #3):" << endl;
 				Vector O = Manager.F3(P, R, S, T);
-				O.Output();
-				cout << endl << "Thread #3 finished" << endl;
+#pragma omp critical
+				{
+					cout << endl << "Vector O (thread #3):" << endl;
+					O.Output();
+					cout << endl << "Thread #3 finished" << endl;
+
+				}
 
 			}
 		}
+#pragma omp barrier 
 	}
 
-
-	std::cout << "Main thread finished" << std::endl;
+	std:: cout << "Main thread finished" << std::endl;
 	system("pause");
 	return 0;
 }
