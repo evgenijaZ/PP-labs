b367d80780f78470891113300b126d808b73056b
evgenijaZ <evg.zubrich@gmail.com>
2017-11-19 22:26:20 +0200

    added 3 functions for threads

--- lab6/lab6/lab6/lab6.cpp
+++ lab6/lab6/lab6/lab6.cpp
@@ -4,9 +4,22 @@
 #include "stdafx.h"
 #include <mpi.h>
 #include <iostream>
+#include "DataClass.h"
+using std::cout;
+using std::cin;
+using std::endl;
+
+int N = 0;
+void F1();
+void F2();
+void F3();
 
 int main(int argc, char **argv)
 {
+	
+	cout << "Main thread started" << endl;
+	cin >> N;
+
 	int ProcNum;
 
 	MPI_Init(&argc, &argv);
@@ -15,4 +28,44 @@ int main(int argc, char **argv)
 	MPI_Finalize();
 	return 0;
 }
+
+
+void F1() {
+	cout << "Thread #1 started" << endl;
+	DataClass Manager(N);
+	Matrix A(N);
+	Matrix D(N);
+	Vector B(N);
+	Matrix E(N);
+	E = Manager.F1(A, D, B);
+	cout << endl << "Matrix E (thread #1):" << endl;
+	E.Output();
+	cout << endl << "Thread #1 finished" << endl;
+
+}
+void F2() {
+	cout << "Thread #2 started" << endl;
+	DataClass Manager(N);
+	Matrix G(N);
+	Matrix K(N);
+	Matrix L(N);
+	Matrix F = Manager.F2(5, G, K, L);
+	cout << endl << "Matrix F (thread #2):" << endl;
+	F.Output();
+	cout << endl << "Thread #2 finished" << endl;
+}
+void F3() {
+
+	cout << "Thread #3 started" << endl;
+	DataClass Manager(N);
+	Matrix P(N);
+	Matrix R(N);
+	Vector S(N);
+	Vector T(N);
+	Vector O = Manager.F3(P, R, S, T);
+	cout << endl << "Vector O (thread #3):" << endl;
+	O.Output();
+	cout << endl << "Thread #3 finished" << endl;
+}
+
 
