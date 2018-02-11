385abce0f8f1fb0d1d524fdc7298d3832c12c04c
evgenijaZ <evg.zubrich@gmail.com>
2018-02-11 22:21:10 +0200

    added body

--- lab7/src/lab1.adb
+++ lab7/src/lab1.adb
@@ -4,26 +4,29 @@
 -- MA = MB * MC + a * ( MK + MT)
 ---------------------------------------
 
-with Ada.Text_IO, Ada.Integer_Text_IO, DataOperations, Ada.Synchronous_Task_Control;
+with Ada
+  .Text_IO, Ada
+  .Integer_Text_IO, DataOperations, Ada
+  .Synchronous_Task_Control;
 use Ada.Text_IO, Ada.Integer_Text_IO, Ada.Synchronous_Task_Control;
 
 procedure Lab1 is
 
-   N:Integer:=100;
-   P:Integer:=2;
-   H:Integer:=N/P;
+   N : Integer := 10;
+   P : Integer := 2;
+   H : Integer := N / P;
 
-   package DataOperations is new DataOperations(N);
-   use DataOperations;
+   package Operations is new DataOperations (N);
+   use Operations;
 
-   MC,MK,MT:Matrix;
+   MC, MK, MT : Matrix;
    --Common resources
-   a:Integer;
-   MB:Matrix;
+   a  : Integer;
+   MB : Matrix;
    --Result
-   MA:Matrix;
+   -- MA:Matrix;
    --Semaphors
-   S1,S2,S3,Sa,Smb:Suspension_Object;
+   S1, S2 : Suspension_Object;
    --Tasks
    procedure Tasks is
       task T1 is
@@ -31,14 +34,17 @@ procedure Lab1 is
       end T1;
       task body T1 is
       begin
-         Put_Line("T1 started");
-         --input
-         FillWithOne(a);
-         FillWithOne(MK);
-         FillWithOne(MC);
-         Set_True(S1);
-         Suspend_Until_True(S2);
-         Put_Line("T1 finished");
+         Put_Line ("T1 started");
+         --input a, MK, MC
+         FillWithOne (a);
+         FillWithOne (MK);
+         FillWithOne (MC);
+         --signal the completion of the input a, MK, MC
+         Set_True (S1);
+         --wait for data input in task T2
+         Suspend_Until_True (S2);
+
+         Put_Line ("T1 finished");
       end T1;
 
       task T2 is
@@ -47,18 +53,23 @@ procedure Lab1 is
 
       task body T2 is
       begin
-         Put_Line("T2 started");
-         FillWithOne(MB);
-         FillWithOne(MT);
-         Set_True(S2);
-         Suspend_Until_True(S1);
-         Put_Line("T2 finished");
+         Put_Line ("T2 started");
+         --input MB, MT
+         FillWithOne (MB);
+         FillWithOne (MT);
+         --signal the completion of the input MB, MT
+         Set_True (S2);
+         --wait for data input in task T1
+         Suspend_Until_True (S1);
+
+
+         Put_Line ("T2 finished");
       end T2;
    begin
       null;
    end Tasks;
 
 begin
-   Put_Line("Program started");
+   Put_Line ("Program started");
    Tasks;
 end Lab1;
