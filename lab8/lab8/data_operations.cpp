8747315fc0a45aa6cd4f640d834340ea7fb3b57b
evgenijaZ <evg.zubrich@gmail.com>
2018-02-23 00:02:28 +0200

    added output

--- lab8/lab8/data_operations.cpp
+++ lab8/lab8/data_operations.cpp
@@ -1,10 +1,13 @@
+#include "stdafx.h"
 #include "data_operations.h"
+#include <iostream>
+#include <iomanip>
 
-matrix create_matrix(int N)
+matrix create_matrix(int n)
 {
-	matrix MA = new vector[N];
-	for (int i = 0; i < N; i++) {
-		MA[i] = new int[N];
+	matrix MA = new vector[n];
+	for (int i = 0; i < n; i++) {
+		MA[i] = new int[n];
 	}
 	return MA;
 }
@@ -25,5 +28,25 @@ void fill_with_one(matrix MA, int n)
 		{
 			MA[i][j] = 1;
 		}
+	}
+}
+
+void output(vector A, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		std::cout << std::setw(5) << A[i];
+	}
+}
+
+void output(matrix MA, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			std::cout << std::setw(5) << MA[i][j];
+		}
+		std::cout << std::endl;
 	}
 }
--- lab8/lab8/data_operations.h
+++ lab8/lab8/data_operations.h
@@ -3,7 +3,11 @@ typedef int* vector;
 typedef int** matrix;
 
 
-matrix create_matrix(int N);
+matrix create_matrix(int n);
 
 void fill_with_one(vector A, int n);
 void fill_with_one(matrix MA, int n);
+
+void output(vector A, int n);
+void output(matrix MA, int n);
+
