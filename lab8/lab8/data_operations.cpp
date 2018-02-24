cf497780c9f7fa2a3fe642e243ff828a65435499
evgenijaZ <evg.zubrich@gmail.com>
2018-02-24 13:55:38 +0200

    added operations

--- lab8/lab8/data_operations.cpp
+++ lab8/lab8/data_operations.cpp
@@ -2,6 +2,7 @@
 #include "data_operations.h"
 #include <iostream>
 #include <iomanip>
+#include <algorithm>    // std::swap
 
 matrix create_matrix(int n)
 {
@@ -35,7 +36,7 @@ void output(vector A, int n)
 {
 	for (int i = 0; i < n; i++)
 	{
-		std::cout << std::setw(5) << A[i];
+		std::cout << std::setw(4) << A[i];
 	}
 }
 
@@ -45,8 +46,79 @@ void output(matrix MA, int n)
 	{
 		for (int j = 0; j < n; j++)
 		{
-			std::cout << std::setw(5) << MA[i][j];
+			std::cout << std::setw(4) << MA[i][j];
 		}
 		std::cout << std::endl;
 	}
+}
+
+matrix multiple(matrix MA, matrix MB, int from, int to, int n)
+{
+	matrix MC = create_matrix(n);
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = from; j < to; j++)
+		{
+			MC[i][j] = 0;
+			for (int k = 0; k < n; k++)
+			{
+				MC[i][j] += MA[i][k] * MB[k][j];
+			}
+		}
+	}
+	//for (int i = 0; i < n; i++)
+	//{
+	//	for (int j = from; j < to; j++)
+	//	{
+	//		MB[i][j] = MC[i][j];
+	//	}
+	//}
+	return MC;
+}
+
+vector multiple(vector A, matrix MB, int from, int to, int n)
+{
+	vector C = new int[n];
+	for (int j = from; j < to; j++)
+	{
+		C[j] = 0;
+		for (int k = 0; k < n; k++)
+		{
+			C[j] += A[k] * MB[k][j];
+		}
+	}
+	return C;
+}
+
+vector multiple(int a, vector B, int from, int to, int n)
+{
+	vector C = new int[n];
+	for (int j = from; j < to; j++)
+	{
+		C[j] = B[j]*a;
+	}
+	return C;
+}
+
+vector amount(vector A, vector B, int from, int to, int n) {
+	vector C = new int[n];
+	for (int j = from; j < to; j++)
+	{
+		C[j] = A[j]+B[j];
+	}
+	return C;
+}
+
+vector sort(vector A, int from, int to) {
+	for (int i = from; i <= to; i++) {
+		for (int j = from; j <= to - i; j++) {
+			if (A[j] < A[j + 1]) {
+				std::swap(A[j], A[j + 1]);
+				std::cout << A[j] << " " << A[j + 1];
+				output(A, 20);
+				std::cout << std::endl;
+			}
+		}
+	}
+	return A;
 }
--- lab8/lab8/data_operations.h
+++ lab8/lab8/data_operations.h
@@ -11,3 +11,9 @@ void fill_with_one(matrix MA, int n);
 void output(vector A, int n);
 void output(matrix MA, int n);
 
+matrix multiple(matrix MA, matrix MB, int from, int to, int n);
+vector multiple(vector A, matrix MB, int from, int to, int n);
+vector multiple(int a, vector B, int from, int to, int n);
+vector amount(vector A, vector B, int from, int to, int n);
+
+vector sort(vector A, int from, int to);
