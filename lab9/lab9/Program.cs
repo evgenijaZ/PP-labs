e964b394dd02e7ffafa8ab756073a7ee3ca48cb2
evgenijaZ <evg.zubrich@gmail.com>
2018-03-21 00:40:32 +0200

    added lab9

---
+++ lab9/lab9/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace lab9
+{
+    class Program
+    {
+      static void Main(string[] args)
+        {
+            Console.WriteLine("started Main");
+            Threads threads = new Threads();
+            threads.Run();
+            Console.WriteLine("finished Main");
+        }
+    }
+}
